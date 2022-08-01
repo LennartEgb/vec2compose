fe2b068053ef2163be8707f52cee7c9071e5941b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-08-01 17:16:09 +0200

    Use smart Regex in PathParser

--- src/main/kotlin/parser/PathParser.kt
+++ src/main/kotlin/parser/PathParser.kt
@@ -1,16 +1,8 @@
 package parser
 
 internal class PathParser(private val commandParser: CommandParser) {
-    fun parse(pathCode: String): List<Command> {
-        if (pathCode.isEmpty()) return emptyList()
-        val (command, rest) = pathCode.partition()
-        val commandValues = rest.indexOfFirstCommand().let { rest.substring(range = 0 until it) }
-        val tail = rest.substring(startIndex = commandValues.length)
-        return commandParser.parse(command + commandValues) + parse(tail)
-    }
-
-    private fun CharSequence.partition() = first() to drop(1)
-    private fun CharSequence.indexOfFirstCommand(): Int =
-        indexOfFirst { it in Command.chars }.takeIf { it != -1 } ?: length
+    private val regex = "[A-z][^A-z]*".toRegex()
+    fun parse(pathCode: String): List<Command> =
+        regex.findAll(pathCode).flatMap { commandParser.parse(it.value) }.toList()
 }
 
