ad017d24706c556c5bdea26d5cab3b0d8b3990c9
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-08-01 21:48:21 +0200

    Added neccessary import statements for kotlin file

--- src/main/kotlin/output/FileOutputStrategy.kt
+++ src/main/kotlin/output/FileOutputStrategy.kt
@@ -7,7 +7,14 @@ class FileOutputStrategy(
 ) : OutputStrategy {
     override fun write(content: String) {
         buildString {
+            append("import androidx.compose.ui.graphics.Color").appendLine()
+            append("import androidx.compose.ui.graphics.PathFillType").appendLine()
+            append("import androidx.compose.ui.graphics.SolidColor").appendLine()
+            append("import androidx.compose.ui.graphics.StrokeCap").appendLine()
+            append("import androidx.compose.ui.graphics.StrokeJoin").appendLine()
             append("import androidx.compose.ui.graphics.vector.ImageVector").appendLine()
+            append("import androidx.compose.ui.graphics.vector.path").appendLine()
+            append("import androidx.compose.ui.unit.dp").appendLine()
             appendLine()
             append(content)
         }.also { File(pathname).apply { writeText(it) }.createNewFile() }
