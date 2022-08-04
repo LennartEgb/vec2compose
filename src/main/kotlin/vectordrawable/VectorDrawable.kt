542a5523862fa4b4531bdd878ed65034f4946d5b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-08-04 19:38:38 +0200

    Support groups and multidimensional groups in VectorDrawable

--- src/main/kotlin/vectordrawable/VectorDrawable.kt
+++ src/main/kotlin/vectordrawable/VectorDrawable.kt
@@ -17,9 +17,28 @@ internal data class VectorDrawable(
     @field:JacksonXmlProperty(localName = "tint")
     val tint: String? = null,
     @field:JacksonXmlElementWrapper(useWrapping = false)
-    val path: List<Path> = emptyList()
+    val path: List<Path> = emptyList(),
+    @field:JacksonXmlElementWrapper(useWrapping = false)
+    val group: List<Group> = emptyList(),
 ) {
+    data class Group(
+        @field:JacksonXmlProperty(localName = "name")
+        val name: String = "",
+        @field:JacksonXmlProperty(localName = "pivotX")
+        val pivotX: Float? = null,
+        @field:JacksonXmlProperty(localName = "pivotY")
+        val pivotY: Float? = null,
+        @field:JacksonXmlProperty(localName = "rotation")
+        val rotation: Float? = null,
+        @field:JacksonXmlElementWrapper(useWrapping = false)
+        val group: List<Group> = emptyList(),
+        @field:JacksonXmlElementWrapper(useWrapping = false)
+        val path: List<Path> = emptyList(),
+    )
+
     data class Path(
+        @field:JacksonXmlProperty(localName = "name")
+        val name: String? = null,
         @field:JacksonXmlProperty(localName = "fillType")
         val fillType: String = "nonZero",
         @field:JacksonXmlProperty(localName = "fillColor")
