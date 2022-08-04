bf1d42fd6ca1ca02d38eb34cffbf0942907d1cf9
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-08-04 19:14:47 +0200

    Added Group functionality to SVGDeserializer

--- src/test/kotlin/svg/SVGDeserializerTest.kt
+++ src/test/kotlin/svg/SVGDeserializerTest.kt
@@ -1,8 +1,8 @@
 package svg
 
-import vectordrawable.XML
 import org.junit.jupiter.api.Test
 import utils.TestObjectMapper
+import vectordrawable.XML
 import kotlin.test.assertEquals
 
 internal class SVGDeserializerTest {
@@ -25,7 +25,10 @@ internal class SVGDeserializerTest {
 
         val expectedPaths = listOf(
             SVG.Path(fillRule = "nonzero", pathData = "M0 0h24v24H0z", fill = "none"),
-            SVG.Path(fillRule = "nonzero", pathData = "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z")
+            SVG.Path(
+                fillRule = "nonzero",
+                pathData = "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            )
         )
         val expected = SVG(
             width = "24px",
@@ -33,6 +36,7 @@ internal class SVGDeserializerTest {
             viewBox = "0 0 24 24",
             fill = "#000000",
             path = expectedPaths,
+            g = emptyList()
         )
 
         assertEquals(
@@ -57,7 +61,10 @@ internal class SVGDeserializerTest {
 
         val expectedPaths = listOf(
             SVG.Path(fillRule = "evenodd", pathData = "M0 0h24v24H0z", fill = "none"),
-            SVG.Path(fillRule = "nonzero", pathData = "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z")
+            SVG.Path(
+                fillRule = "nonzero",
+                pathData = "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            )
         )
         val expected = SVG(
             width = "24px",
@@ -65,11 +72,48 @@ internal class SVGDeserializerTest {
             viewBox = "0 0 24 24",
             fill = "#000000",
             path = expectedPaths,
+            g = emptyList(),
         )
 
         assertEquals(
             expected = Result.success(expected),
             actual = deserializer.deserialize(content)
+        )
+    }
+
+    @Test
+    fun `parse SVG with groups`() {
+        // NOTE: SVG found here: https://www.sarasoueidan.com/blog/structuring-grouping-referencing-in-svg/
+        val content = """
+            <svg width="1144.12px" height="400px" viewBox="0 0 572.06 200">
+            	<g id="bird">
+            		<g id="body">
+            			<path id="first" d="M48.42,78.11"/>
+            		</g>
+            		<g id="head">
+            			<path id="second" d="M48.42,78.11"/>
+            		</g>
+            	</g>
+            </svg>
+        """.trimIndent()
+        assertEquals(
+            expected = SVG(
+                width = "1144.12px",
+                height = "400px",
+                viewBox = "0 0 572.06 200",
+                path = emptyList(),
+                g = listOf(
+                    SVG.Group(
+                        name = "bird",
+                        path = listOf(),
+                        g = listOf(
+                            SVG.Group(name = "body", path = listOf(SVG.Path(id = "first",pathData = "M48.42,78.11"))),
+                            SVG.Group(name = "head", path = listOf(SVG.Path(id = "second",pathData = "M48.42,78.11")))
+                        )
+                    )
+                )
+            ),
+            actual = deserializer.deserialize(XML(content)).getOrThrow()
         )
     }
 }
