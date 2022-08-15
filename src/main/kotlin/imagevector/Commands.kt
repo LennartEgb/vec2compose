d3fcb39c5db32720561625fe950096acbf32870b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-08-15 08:09:51 +0200

    Adjusted Commands extension params creation

--- src/main/kotlin/imagevector/Commands.kt
+++ src/main/kotlin/imagevector/Commands.kt
@@ -21,13 +21,9 @@ internal val Command.methodName: String
 internal val Command.methodParams: String
     get() = when (this) {
         Command.Close -> ""
-        is Command.ArcTo -> "${horizontalEllipseRadius}f, " +
-                "${verticalEllipseRadius}f, " +
-                "${theta}f, " +
-                "$isMoreThanHalf, " +
-                "$isPositiveArc, " +
-                "${x1}f, " +
-                "${y1}f"
+        is Command.ArcTo -> floatParams(horizontalEllipseRadius, verticalEllipseRadius, theta) +
+                ", $isMoreThanHalf, $isPositiveArc, " +
+                floatParams(x1, y1)
         is Command.HorizontalLineTo -> "${x}f"
         is Command.VerticalLineTo -> "${y}f"
         is Command.CurveTo -> floatParams(x1, y1, x2, y2, x3, y3)
