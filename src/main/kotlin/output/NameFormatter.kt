f386c923847339df1e07959a10bddf18005e1853
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-01-02 12:00:11 +0100

    Pass Locale to NameFormatter

--- src/main/kotlin/output/NameFormatter.kt
+++ src/main/kotlin/output/NameFormatter.kt
@@ -2,7 +2,7 @@ package output
 
 import java.util.*
 
-internal class NameFormatter {
+internal class NameFormatter(private val locale: Locale = Locale.getDefault()) {
     private val regex = "[-_ ]".toRegex()
 
     fun format(name: String): String {
@@ -11,7 +11,7 @@ internal class NameFormatter {
             endIndex = name.indexOf(char = '.').takeIf { it != -1 } ?: name.indices.last
         )
         return nameWithoutExtension.split(regex).joinToString(separator = "") { word ->
-            word.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+            word.replaceFirstChar { if (it.isLowerCase()) it.titlecase(locale) else it.toString() }
         }
     }
 }
