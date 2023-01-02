276fc3d0071b3db848d97bdb90bfd3afc897a9c7
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-01-02 11:53:57 +0100

    Fix unit tests and file names

--- src/main/kotlin/output/FileOutputStrategy.kt
+++ src/main/kotlin/output/FileOutputStrategy.kt
@@ -2,7 +2,6 @@ package output
 
 import imagevector.ImageVectorImportProvider
 import java.io.File
-import java.lang.StringBuilder
 
 internal class FileOutputStrategy(
     private val name: String,
@@ -11,24 +10,25 @@ internal class FileOutputStrategy(
 ) : OutputStrategy {
     override fun write(content: String) {
         buildString {
-            importProvider.createImports().forEach { append(it).appendLine() }
+            importProvider.createImports().forEach { appendLine(it) }
             appendLine()
-
-            append("private var cache: ImageVector? = null").appendLine()
-            append("val $name: ImageVector").appendLine()
-
-            val lines = content.lines()
-            lines.forEach { line ->
-                if (line == lines.first()) {
-                    indent().append("get() = cache ?: $line")
-                } else {
-                    indent().append(line)
-                }
-                appendLine()
+            appendLine("private var cache: ImageVector? = null")
+            appendLine("val $name: ImageVector")
+            content.lines().forEachMeta { line, isFirst, isLast ->
+                indent()
+                if (isFirst) append("get() = cache ?: ")
+                append(line)
+                if (isLast) appendLine()
             }
             append(".also { cache = it }")
         }.also { File(pathname).apply { writeText(it) }.createNewFile() }
     }
 
     private fun StringBuilder.indent() = append("    ")
+
+    private fun <T> List<T>.forEachMeta(action: (T, isFirst: Boolean, isLast: Boolean) -> Unit) {
+        val firstIndex = indices.first
+        val lastIndex = indices.last
+        forEachIndexed { index, t -> action(t, index == firstIndex, index == lastIndex) }
+    }
 }
--- src/main/kotlin/output/NameFormatter.kt
+++ src/main/kotlin/output/NameFormatter.kt
@@ -6,7 +6,10 @@ internal class NameFormatter {
     private val regex = "[-_ ]".toRegex()
 
     fun format(name: String): String {
-        val nameWithoutExtension = name.substring(startIndex = 0, endIndex = name.indexOf(char = '.'))
+        val nameWithoutExtension = name.substring(
+            startIndex = 0,
+            endIndex = name.indexOf(char = '.').takeIf { it != -1 } ?: name.indices.last
+        )
         return nameWithoutExtension.split(regex).joinToString(separator = "") { word ->
             word.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
         }
