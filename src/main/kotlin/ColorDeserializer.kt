edac1281579e5d157984a5b4f41f047a46788079
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-02-14 22:26:05 +0100

    Reformat ColorDeserializer

--- src/main/kotlin/ColorDeserializer.kt
+++ src/main/kotlin/ColorDeserializer.kt
@@ -1,18 +1,9 @@
 internal class ColorDeserializer {
     fun deserialize(color: String): VectorSet.Path.FillColor {
-        if (color.startsWith("#")) {
-            return deserialize(color.drop(1))
-        }
+        if (color.startsWith("#")) return deserialize(color.drop(1))
+        if (color.length == 3) return deserialize(color.fold(initial = "") { acc, char -> "$acc$char$char" })
+        if (color.length == 6) return deserialize("FF$color")
 
-        if (color.length == 3) {
-            return deserialize(color.fold(initial = "") { acc, char -> "$acc$char$char" })
-        }
-
-        if (color.length == 6) {
-            return deserialize("FF$color")
-        }
-
-        // FFAABBCC
         require(color.length == 8)
         val (a, r, g, b) = color.windowed(size = 2, step = 2)
         return VectorSet.Path.FillColor(
