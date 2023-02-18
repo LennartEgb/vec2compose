cdd25f644a08b524c7dbe6b86ea2b7be4e5563f3
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-02-18 15:08:13 +0100

    Add ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
     application
 }
 
