25a26766848c88c6b90889507dc5ed563a16d7cf
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-02-18 21:47:56 +0100

    Add tests for Arguments and FileOutputStrategy

---
+++ src/test/kotlin/ArgumentsTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ArgumentsTest {
+
+    @Test
+    fun getInput() {
+        val arguments = Arguments(arrayOf("-i", "input.svg"))
+        assertEquals(expected = "input.svg", actual = arguments.input)
+    }
+
+    @Test
+    fun getOutput() {
+        val arguments = Arguments(arrayOf("-i", "input.svg", "-o", "Output.kt"))
+        assertEquals(expected = "Output.kt", actual = arguments.output)
+    }
+}
