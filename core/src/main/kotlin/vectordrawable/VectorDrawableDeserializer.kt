b4138ee4c5922f7d1339ec7a945134fadf830b70
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-07-10 18:03:41 +0200

    Use kotlinx.serialization on VectorDrawable and try to get it to work

--- core/src/main/kotlin/vectordrawable/VectorDrawableDeserializer.kt
+++ core/src/main/kotlin/vectordrawable/VectorDrawableDeserializer.kt
@@ -1,9 +1,11 @@
 package vectordrawable
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import nl.adaptivity.xmlutil.serialization.XML
 
 internal class VectorDrawableDeserializer(private val mapper: ObjectMapper) {
+
     fun deserialize(content: String): Result<VectorDrawable> {
-        return runCatching { mapper.readValue(content, VectorDrawable::class.java) }
+        return runCatching { XML.decodeFromString<VectorDrawable>(content) }
     }
 }
