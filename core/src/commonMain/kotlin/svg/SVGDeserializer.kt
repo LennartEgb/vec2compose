7fa8609a4612944bf55d8335895f296560f222d5
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-02-05 09:46:13 +0100

    Updated xmlutil version and adjust SVGDeserializer

--- core/src/commonMain/kotlin/svg/SVGDeserializer.kt
+++ core/src/commonMain/kotlin/svg/SVGDeserializer.kt
@@ -2,17 +2,18 @@ package svg
 
 import nl.adaptivity.xmlutil.ExperimentalXmlUtilApi
 import nl.adaptivity.xmlutil.serialization.DefaultXmlSerializationPolicy
+import nl.adaptivity.xmlutil.serialization.UnknownChildHandler
 import nl.adaptivity.xmlutil.serialization.XML
 
 internal class SVGDeserializer {
 
     @OptIn(ExperimentalXmlUtilApi::class)
     private val xmlConfig = XML {
-        policy = DefaultXmlSerializationPolicy(
-            pedantic = false,
-            unknownChildHandler = { _, _, _, _, _ -> emptyList() }
-        )
-        this.repairNamespaces
+        policy = DefaultXmlSerializationPolicy.Builder().apply {
+            pedantic = false
+            unknownChildHandler = UnknownChildHandler { _, _, _, _, _ -> emptyList() }
+            repairNamespaces = true
+        }.build()
     }
 
     fun deserialize(content: String): Result<SVG> = runCatching {
