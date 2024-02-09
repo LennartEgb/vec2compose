c3dea04037e177436aa3020ef0236ab58d0f5609
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-02-09 23:27:38 +0100

    Merge lines in ImageVectorParser

--- core/src/commonMain/kotlin/dev/lennartegb/vec2compose/core/imagevector/ImageVectorParser.kt
+++ core/src/commonMain/kotlin/dev/lennartegb/vec2compose/core/imagevector/ImageVectorParser.kt
@@ -3,18 +3,33 @@ package dev.lennartegb.vec2compose.core.imagevector
 import dev.lennartegb.vec2compose.core.VectorSet
 
 class ImageVectorParser(indentation: CharSequence) {
+
     private val emptyLineRegex = "(?m)^[ \t]*\r?\n".toRegex()
     private val composeMethodCreator = ComposeMethodCreator(indentation)
 
     fun parse(name: String, vectorSet: VectorSet): String {
         return buildString {
             append(composeMethodCreator.parseConstructor(name, vectorSet))
-            vectorSet.groups.map(composeMethodCreator::parseGroup).forEach(::appendLine)
-            vectorSet.paths.map(composeMethodCreator::parsePath).forEachIndexed { index, s ->
-                append(s)
-                if (index != vectorSet.paths.indices.last) appendLine()
+
+            val groups = vectorSet.groups.mergeLines(composeMethodCreator::parseGroup)
+            val groupIndices = groups.indices
+            groups.forEachIndexed { index, line ->
+                append(line)
+                if (index != groupIndices.last) appendLine()
             }
+
+            val paths = vectorSet.paths.mergeLines(composeMethodCreator::parsePath)
+            val pathIndices = paths.indices
+            paths.forEachIndexed { index, s ->
+                append(s)
+                if (index != pathIndices.last) appendLine()
+            }
+
             append(".build()")
         }.replace(emptyLineRegex, "")
+    }
+
+    private fun <T> List<T>.mergeLines(transform: (T) -> String): List<String> {
+        return joinToString(separator = "", transform = transform).lines()
     }
 }
