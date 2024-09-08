08d33a79093a15502146847ec3813e4ae5576e2b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-09-09 09:37:55 +1000

    move cli classes to commonMain

--- cli/src/nativeMain/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-import org.koin.core.context.startKoin
-
-fun main(args: Array<String>) {
-    val arguments = Arguments(args)
-    startKoin { modules(cliModule) }
-    
-    val indentation = "    "
-    Application(indentation = indentation).run(arguments)
-}
