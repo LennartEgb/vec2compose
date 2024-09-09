fb5fadcc7a2c39ff026e9dea401d472202a11e54
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-09-09 15:50:08 +1000

    add ktlint to all submodules and use intellij codestyle in editorconfig

--- vector-drawable/build.gradle.kts
+++ vector-drawable/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.kover)
+    alias(libs.plugins.ktlint)
 }
 
 kotlin {
