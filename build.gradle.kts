261d2486269ac33991ff6ae0803d0ed517b2a154
Lennart Egbers <26793300+LennartEgb@users.noreply.github.com>
2024-09-11 12:00:23 +1000

    Gradle: setup scripts (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,23 +12,25 @@ dependencies {
     kover(project(":cli"))
 }
 
-tasks.register<Copy>("assembleCli") {
-    val os = getHostOs()
-    dependsOn(":cli:${os}Binaries")
+tasks {
+    register<Copy>("assembleCli") {
+        val systemOs = System.getProperty("os.name")
+        val os = when {
+            systemOs == "Mac OS X" -> "macosX64"
+            systemOs == "Linux" -> "linuxX64"
+            systemOs.startsWith("Windows") -> "mingwX64"
+            else -> throw GradleException("Host OS is not supported.")
+        }
 
-    from("${project.projectDir}/cli/build/bin/$os/releaseExecutable/cli.kexe") {
-        rename { project.name }
+        dependsOn(":cli:${os}Binaries")
+
+        from("${project.projectDir}/cli/build/bin/$os/releaseExecutable/cli.kexe") {
+            rename { project.name }
+        }
+        into("${project.rootDir}/dist")
     }
-    into("${project.rootDir}/dist")
-}
 
-fun getHostOs(): String {
-    val systemOs = System.getProperty("os.name")
-    val isMingwX64 = systemOs.startsWith("Windows")
-    return when {
-        systemOs == "Mac OS X" -> "macosX64"
-        systemOs == "Linux" -> "linuxX64"
-        isMingwX64 -> "mingwX64"
-        else -> throw GradleException("Host OS is not supported.")
+    register<Exec>("setupGitHooks") {
+        commandLine("sh", ".scripts/setupGitHooks")
     }
 }
