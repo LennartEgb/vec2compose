516475e91954d2bcff8fc507904b80587bffc9c7
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-09-11 17:56:11 +1000

    set minimium dimension of window

--- compose/src/desktopMain/kotlin/main.kt
+++ compose/src/desktopMain/kotlin/main.kt
@@ -1,14 +1,19 @@
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import dev.lennartegb.vec2compose.app.App
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "vec2compose"
     ) {
+        LaunchedEffect(Unit) {
+            window.minimumSize = Dimension(400, 300)
+        }
         App(modifier = Modifier.fillMaxSize())
     }
 }
