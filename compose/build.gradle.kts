93d86cad9d416e9d70ce073bf09d6f534646a057
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-09-14 15:06:59 +1000

    add ktlint to compose module

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ktlint)
 }
 
 kotlin {
