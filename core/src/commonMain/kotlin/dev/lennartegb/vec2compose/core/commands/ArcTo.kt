5b2523aee16e2e43f18748f97ef81246521ad747
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-11-30 21:21:55 +0100

    Run ktlintFormat to update with ktlint update

--- core/src/commonMain/kotlin/dev/lennartegb/vec2compose/core/commands/ArcTo.kt
+++ core/src/commonMain/kotlin/dev/lennartegb/vec2compose/core/commands/ArcTo.kt
@@ -7,8 +7,7 @@ data object ArcTo : CommandSpec {
     override val name: String = "arcTo"
     override val argsCount: Int = 7
 
-    override fun getArguments(args: List<String>): String =
-        "${args[0]}f, ${args[1]}f, ${args[2]}f, ${args[3] == "1"}, ${args[4] == "1"}, ${args[5]}f, ${args[6]}f"
+    override fun getArguments(args: List<String>): String = "${args[0]}f, ${args[1]}f, ${args[2]}f, ${args[3] == "1"}, ${args[4] == "1"}, ${args[5]}f, ${args[6]}f"
 
     operator fun invoke(
         horizontalEllipseRadius: Float,
@@ -18,11 +17,10 @@ data object ArcTo : CommandSpec {
         isPositiveArc: Boolean,
         x1: Float,
         y1: Float
-    ): Command =
-        Command(
-            "A$horizontalEllipseRadius,$verticalEllipseRadius,$theta " +
-                "${isMoreThanHalf.toNum()},${isPositiveArc.toNum()} $x1 $y1"
-        )
+    ): Command = Command(
+        "A$horizontalEllipseRadius,$verticalEllipseRadius,$theta " +
+            "${isMoreThanHalf.toNum()},${isPositiveArc.toNum()} $x1 $y1"
+    )
 
     private fun Boolean.toNum() = if (this) "1" else "0"
 }
