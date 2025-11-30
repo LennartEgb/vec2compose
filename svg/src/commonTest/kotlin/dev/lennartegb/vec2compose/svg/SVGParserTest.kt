a3e482fe1f295886af58839dbb03f0d083ecb792
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-11-30 21:09:42 +0100

    Move svg and testSvg functions to private functions in test classes

--- svg/src/commonTest/kotlin/dev/lennartegb/vec2compose/svg/SVGParserTest.kt
+++ svg/src/commonTest/kotlin/dev/lennartegb/vec2compose/svg/SVGParserTest.kt
@@ -17,6 +17,25 @@ import kotlin.test.assertFailsWith
 
 internal class SVGParserTest {
 
+    private fun svg(
+        width: String = "24px",
+        height: String = "24px",
+        viewBox: String? = "0 0 24 24",
+        fill: String = "#000000",
+        block: () -> String
+    ): String {
+        return buildString {
+            // NOTE: The supported SVGs must have the documented namespace: https://www.w3.org/TR/SVG/struct.html#Namespace
+            appendLine("""<svg xmlns="http://www.w3.org/2000/svg"""")
+            appendLine("""height="$height"""")
+            appendLine("""width="$width"""")
+            if (viewBox != null) appendLine("""viewBox="$viewBox"""")
+            appendLine("""fill="$fill">""")
+            appendLine(block())
+            appendLine("</svg>")
+        }
+    }
+
     private val ImageVector.groups: List<ImageVector.Group>
         get() = nodes.filterIsInstance<ImageVector.Group>()
 
